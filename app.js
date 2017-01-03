ddf90b29bec378d94095ce7d4d2a50e53da5e7f2
小齐 <705764065@qq.com>
2017-01-04 00:03:19 +0800

    test use session

--- app.js
+++ app.js
@@ -2,14 +2,35 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import BodyParser from 'body-parser';
 import path from 'path';
+var session =require('express-session') ;
+//var redisStore =require('connect-redis')(session);
 const app =express();
 const port =3000;
 app.use(cookieParser());
 app.use(BodyParser());
+app.use(session({
+    //store:new redisStore(),
+    secret:'somesecretoken',
+    resave:false,
+    saveUninitialized:true,
+    cookie:{secure:true}
+
+}));
 app.get('/',(req,res)=>{
     res.set({'Accept-Charset':'utf-8'});
-    res.cookie('isVisit',true,{maxAge:60*1000});
-    res.sendFile(path.join(__dirname,'/index.html'));
+    console.log(req.session);
+    res.render(path.join(__dirname,'/index.html'),{h1:'hello'});
+   /*
+   *  if(req.session.isVisit){
+    req.session.isVisit++;
+    res.send(`<p>第${isVisit}次来到此页面`)
+    }else {
+    req.session.isVisit=1;
+    res.send('欢迎第一次访问此页面')
+    }
+   * */
+   // res.cookie('isVisit',true,{maxAge:60*1000});
+    //
 
 });
 app.listen(port,(error)=>{
--- run.js
+++ run.js
@@ -2,4 +2,4 @@
  * Created by xiaoqi on 2017/1/2.
  */
 require('babel-register');
-require('./app.js')
+require('./app.js');
